3c0a0a2be19c26400160e434ba2995e7f6b0326f
Admin <conner.p.mccabe@gmail.com>
2020-01-22 17:34:42 -0600

    getting started

---
+++ src/App/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  state = {
+    todos: []
+  }
+}
+render() {
+  return (
+    <div>
+      <h1>Comming Soon</h1>
+    </div>
+  );
+}
+
+export default App;
