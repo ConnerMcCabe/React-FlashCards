ce9b0ba7bb949cee775b19b968d8f2ad0d80c896
Admin <conner.p.mccabe@gmail.com>
2020-01-22 18:02:16 -0600

    adding delete and button for li

--- src/App/App.js
+++ src/App/App.js
@@ -27,12 +27,30 @@ class App extends Component {
     })
   }
 
+  handleDelete = id => {
+    let currState = this.state.todos
+    let newState = currState.filter(item => currState[id] !== item )
+    this.setState({
+      todos : newState
+    })
+  };
 
   render() {
-    
+    const renderTodos = this.state.todos.map((item, index) => {
+      return (
+        <form>
+          <li>
+            {item}
+            <button>
+              Delete
+            </button>
+          </li>
+        </form>
+      )
+    })
     return (
       <div className="App">
-        <ul className="todos">  </ul>
+        <ul className="todos"> {renderTodos} </ul>
         <form className="add-form" onSubmit={this.handleSubmit}>
           <input type="text" onChange={this.handleOnChange} value={this.state.inputValue}/>
           <input type="submit" value="Add"/>
