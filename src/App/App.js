a0e91c91ae58a02c2066c10c3562cf6cd84cd4de
Conner McCabe <conner.p.mccabe@gmail.com>
2020-06-10 16:09:47 -0500

    file structure set up

--- src/App/App.js
+++ src/App/App.js
@@ -1,62 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  state = {
-    todos: []
-  }
-
-  handleCreate = todo => {
-    this.setState({
-      todos : [todo, ...this.state.todos],
-      inputValue : ''
-    })
-  }
-
-  handleSubmit = event => {
-    event.preventDefault()
-    this.handleCreate(this.state.inputValue)
-    this.setState({
-      inputValue : ''
-    })
-  }
-
-  handleOnChange = event => {
-    this.setState({
-      inputValue : event.target.value
-    })
-  }
-
-  handleDelete = id => {
-    let currState = this.state.todos
-    let newState = currState.filter(item => currState[id] !== item )
-    this.setState({
-      todos : newState
-    })
-  };
-
-  render() {
-    const renderTodos = this.state.todos.map((item, index) => {
-      return (
-        <form>
-          <li key={index} className="todo">
-            {item}
-            <button onClick={() => this.handleDelete(index)}>
-              Delete
-            </button>
-          </li>
-        </form>
-      )
-    })
-    return (
-      <div className="App">
-        <ul className="todos"> {renderTodos} </ul>
-        <form className="add-form" onSubmit={this.handleSubmit}>
-          <input type="text" onChange={this.handleOnChange} value={this.state.inputValue}/>
-          <input type="submit" value="Add"/>
-        </form>
-      </div>
-    );
-  }
-}
-export default App;
+export default function App() {
+  return (
+    <div>test</div>
+  )
+};
