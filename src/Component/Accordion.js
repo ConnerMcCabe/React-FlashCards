53f572af4658558d8d29668361f63e25f84ded75
Conner McCabe <conner.p.mccabe@gmail.com>
2020-06-10 19:29:24 -0500

    adding chevron component to accordian

--- src/Component/Accordion.js
+++ src/Component/Accordion.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import Chevron from './chevron';
 import './Accordion';
 
 function Accordion(props) {
@@ -14,9 +14,13 @@ function Accordion(props) {
         <div className="accordionSection">
             <button className={`accordion ${setActive}`} onClick={toggleAccordion}>
                 <p className="accordionTitle">{props.title}</p>
+            <Chevron width={10} fill={'#777'}/>
             </button>
             <div className="accordionContent"></div>
-            <div className="accordionText" dangerouslySetInnerHTML={{ __html: props.content}} />
+            <div 
+                className="accordionText" 
+                dangerouslySetInnerHTML={{ __html: props.content}} 
+            />
         </div>
     )
 };
