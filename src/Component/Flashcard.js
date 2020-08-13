5d0c766bdae9ff69c8e4150acee65dd68ecb2529
Conner McCabe <conner.p.mccabe@gmail.com>
2020-08-13 12:02:36 -0500

    adding flashcard, list

---
+++ src/Component/Flashcard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Flashcard';
+
+function Flashcard() {
+    
+    return (
+        <div className="Flashcard">
+            
+        </div>
+    )
+};
+
+export default Flashcard;
