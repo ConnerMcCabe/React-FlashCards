4d510d51a9f5b2977f7248aa292e66f317821cb6
Conner McCabe <conner.p.mccabe@gmail.com>
2020-08-13 11:55:14 -0500

    getting started

--- src/App/App.js
+++ src/App/App.js
@@ -1,31 +1,10 @@
 import React from 'react';
-import Accordion from '../Component/Accordion';
 import './App.css';
 
 export default function App() {
   return (
     <div>
-      <Accordion 
-        title="What is your return policy"
-        content="lorem ipsum"
-      />
-      <Accordion 
-        title="What is your return policy"
-        content="
-          <p>lorem ipsum kappa kappa </p>
-          <p>lorem ipsum kappa kappa </p>
-        "
-      />
-      <Accordion 
-        title="What is your return policy"
-        content="
-          <p>lorem ipsum kappa slappa </p>
-          </br>
-          <p>lorem ipsum kappa slappa </p>
-          </br>
-          <p>lorem ipsum kappa slappa </p>
-        "
-      />
+      test
     </div>
   )
 };
