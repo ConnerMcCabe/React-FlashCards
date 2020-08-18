4ae458e94cf966e3d3b039c28af126bcf54cd27e
Conner McCabe <conner.p.mccabe@gmail.com>
2020-08-18 14:36:18 -0500

    getting started

--- src/App/App.js
+++ src/App/App.js
@@ -11,9 +11,18 @@ function App() {
     axios
       .get('https://opentdb.com/api.php?amount=10')
       .then(res => {
-        console.log(res.data)
+        setFlashcards(res.data.results.map((questionItem, index) => {
+          const answer = questionItem.correct_answer
+          const options = [...questionItem.incorrect_answers, answer]
+          return {
+            id: `${index}-${Date.now()}`,
+            question: questionItem.question,
+            answer: answer,
+            options: options.sort(() => Math.random() - .5)
+          }
+        }))
       })
-  })
+  }, [])
 
   return (
     <div>
