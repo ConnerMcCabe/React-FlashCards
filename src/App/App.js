21018b3fdf4a90c9582b65fe86882c03b3173ee4
Conner McCabe <conner.p.mccabe@gmail.com>
2021-02-10 15:23:44 -0600

    adding bug fix for html encoded strings

--- src/App/App.js
+++ src/App/App.js
@@ -5,18 +5,21 @@ import axios from "axios"
 
 function App() {
 
-  const [flashcards, setFlashcards] = useState(sampleInfo)
+  const [flashcards, setFlashcards] = useState([])
 
   useEffect(() => {
     axios
       .get('https://opentdb.com/api.php?amount=10')
       .then(res => {
         setFlashcards(res.data.results.map((questionItem, index) => {
-          const answer = questionItem.correct_answer
-          const options = [...questionItem.incorrect_answers, answer]
+          const answer = decodeString(questionItem.correct_answer)
+          const options = [
+            ...questionItem.incorrect_answers.map(a => decodeString(a)), 
+            answer
+          ]
           return {
             id: `${index}-${Date.now()}`,
-            question: questionItem.question,
+            question: decodeString(questionItem.question),
             answer: answer,
             options: options.sort(() => Math.random() - .5)
           }
@@ -24,37 +27,18 @@ function App() {
       })
   }, [])
 
+  function decodeString(str) {
+    const textArea = document.createElement('textarea')
+    textArea.innerHTML = str
+    return textArea.value
+  }
+
   return (
     <div>
       <FlashcardList flashcards={flashcards} />
     </div>
   )
 };
-
-
-const sampleInfo = [
-  {
-      id: 1,
-      question: 'what is 2 + 2',
-      answer: '4',
-      options: [
-          '1',
-          '3',
-          '7',
-          '4'
-      ]
-  }, {
-      id: 2,
-      question: 'Question 2',
-      answer: 'answer',
-      options: [
-          'wrong',
-          'right',
-          'wrong',
-          'wrong'
-      ]
-  }
-]
 
 
 export default App
